b4e4e78fa6df617730c8416f0c8b18657a3f914e
annmutemi <annshiru92@gmail.com>
2024-09-14 16:47:21 +0300

    react-hooks-components-props-mini-project final code

--- src/__tests__/About.test.js
+++ src/__tests__/About.test.js
@@ -28,3 +28,16 @@ test("renders a <p> with the about text", () => {
   expect(p).toBeInTheDocument();
   expect(p.tagName).toBe("P");
 });
+test("uses a default value for the image if no image is passed as a prop", () => {
+  render(<About />);
+  const img = screen.queryByAltText("blog logo");
+  expect(img).toBeInTheDocument();
+  expect(img.src).toContain("https://via.placeholder.com/215");
+});
+
+test("renders a <p> with the about text", () => {
+  render(<About about="About this blog" />);
+  const p = screen.queryByText("About this blog");
+  expect(p).toBeInTheDocument();
+  expect(p.tagName).toBe("P");
+});
--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,14 @@
-import React from 'react'
+import React from "react";
+//import blogData from "../data/blog";
+//import logo from "../assets/logo";
 
-function About({image,about}) {
-  return (
-    <div>
-    <aside>
-       <img
-       src={image}
-       alt='blog logo'
-       />
-       <p>{about}</p>
-    </aside>
-    </div>
-  )
-}
+function About ({image="https://via.placeholder.com/215", about}) {
+    return (
+        <aside>
+            <img src={image} alt="blog logo"></img>
+            <p>{about}</p>
+        </aside>
+    )
+};
 
-export default About
+export default About;
